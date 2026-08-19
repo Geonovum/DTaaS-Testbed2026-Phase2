0b5538a288c591ec9b47e9435c774a6fc64462eb
Niels Hoffmann <niels.hoffmann@hotmail.com>
2026-08-19 10:42:20 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # DTaaS-Testbed2026
 repository for wiki, engineering report and documentation of the DTaaS Testbed 2026
+
+We expect submissions to this repository adhere to the cc-by license:
+
+https://creativecommons.org/licenses/by/4.0/
+
+---
+
+## EngineeringReport
+
+A respec document reporting the goal and learnings from the Testbed.
+
+https://geonovum.github.io/DTaaS-Testbed2026/
+
+
+## Wiki
+
+The Wiki can be used to document various topics addressed in the Testbed.
+
+https://github.com/Geonovum/DTaaS-Testbed2026/wiki
+
+
+## Discussions
+
+https://github.com/Geonovum/DTaaS-Testbed2026/discussions
+
+
+
---
+++ docs/js/document_config.js
@@ -0,0 +1,45 @@
+var documentConfig =
+{
+  title: "DTaaS Testbed2026",
+  subtitle: "Engineering Report",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by",
+  maxTocLevel: 3,
+//   publishDate: "2024-06-21",
+  publishVersion: "0.1.0",
+  thisVersion: [
+    "https://geonovum.github.io/DTaaS-Testbed2026"
+  ],
+  github: "https://github.com/geonovum/DTaaS-Testbed2026/",
+  issueBase: "https://github.com/geonovum/DTaaS-Testbed2026/issues/",
+  edDraftURI: "https://geonovum.github.io/DTaaS-Testbed2026 ",
+  authors: [
+    {
+      name: "Bart De Lathouwer",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Niels Hoffmann",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Niels Hoffmann",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+
+  localBiblio: {
+        
+    NLDT: {
+      title: "Referentie Architectuur NLDT",
+      href: "https://geonovum.github.io/NLDT-Architectuur/",
+      publisher: "Geonovum"
+  }
+  }
+}
